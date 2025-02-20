1ec146827632ab1396c0225de169874656b1bb7c
izaan.noman <izaan.nyc@gmail.com>
2025-02-20 04:12:55 -0500

    Show N/A for games without a metacritic score

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,11 +5,18 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  let color =
+    score === undefined || score === null
+      ? "grey"
+      : score > 75
+      ? "green"
+      : score > 60
+      ? "yellow"
+      : "";
 
   return (
     <Badge colorPalette={color} fontSize="14px" paddingX={2} borderRadius="4px">
-      {score}
+      {score ? score : "N/A"}
     </Badge>
   );
 };
